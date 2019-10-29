deb9fd567b0c9d9900a644a8dcfb5004458e6647
Wesley Greenwood <Daedun@gmail.com>
2019-10-29 17:17:42 -0400

    added process helper

---
+++ MaTM/helpers/process.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+import typing
+
+
+def get_output(args: typing.List[str]):
+    return subprocess.check_output(args)\
+        .decode('utf-8').rstrip()
+
+
+def get_list_output(args: typing.List[str]):
+    return filter(
+        None,
+        get_output(args).split('\n')
+    )
+
+
+def run(args: typing.List[str], hide_output=True) -> int:
+    kwargs = {}
+    if hide_output:
+        dnull = open(os.devnull, 'wt')
+        kwargs['stdout'] = dnull
+        kwargs['stderr'] = dnull
+    subprocess.call(
+        args,
+        **kwargs
+    )
