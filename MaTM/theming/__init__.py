026d263f02384d0596a269b3a0a037400fe5a00e
Wesley Greenwood <Daedun@gmail.com>
2019-10-29 23:32:46 -0400

    Included base spicetify in create_default_manager

--- MaTM/theming/__init__.py
+++ MaTM/theming/__init__.py
@@ -6,7 +6,11 @@ from MaTM.theming.manager import ThemeManager  # noqa:F401
 def create_default_manager() -> ThemeManager:
     tm = ThemeManager()
 
-    from MaTM.theming.handlers import PolybarThemeHandler
+    from MaTM.theming.handlers import (
+        PolybarThemeHandler,
+        SpicetifyThemeHandler
+    )
     tm.add_theme_handler(PolybarThemeHandler())
+    tm.add_theme_handler(SpicetifyThemeHandler())
 
     return tm
--- MaTM/theming/handlers/__init__.py
+++ MaTM/theming/handlers/__init__.py
@@ -1,1 +1,2 @@
 from MaTM.theming.handlers.polybar import PolybarThemeHandler  # noqa:F401
+from MaTM.theming.handlers.spicetify import SpicetifyThemeHandler  # noqa:F401
