e7ca62cbfcc3428bdf059f52c730e56bb0d4bda3
Wesley Greenwood <Daedun@gmail.com>
2019-10-31 18:22:35 -0400

    get_path: used to locate a program. [config/which]

--- MaTM/helpers/files.py
+++ MaTM/helpers/files.py
@@ -1,3 +1,5 @@
+from os import path
+import shutil
 import typing
 
 
@@ -11,3 +13,13 @@ def write_lines(filename: str, lines: typing.Iterable[str]):
     with open(filename, 'wt') as f:
         for line in lines:
             print(line, file=f)
+
+
+def get_path(section, program: str) -> str:
+    program_path = section[program] if program in section\
+        else shutil.which(program)
+    if program_path is not None and\
+            len(program_path) > 0 or\
+            path.isfile(program_path):
+        return program_path
+    return None
