aa0b82f93dbd6b1d0ab4cf35dea2fc178da237c7
Wesley Greenwood <Daedun@gmail.com>
2019-10-31 23:05:44 -0400

    Finished polybar theme implementation

--- MaTM/theming/handlers/polybar.py
+++ MaTM/theming/handlers/polybar.py
@@ -1,35 +1,52 @@
 from configparser import ConfigParser
 from os import path
-import shutil
 
 from MaTM.helpers import environ, process
 from MaTM.theming import AppThemeManager, ThemeManager
 
+_KEY = 'polybar'
+
 
 class PolybarThemeHandler(AppThemeManager):
-    __appname__ = 'polybar'
+    __appname__ = _KEY
+    polybar_dir: str
+    launch_script: str
 
     def __init__(self):
         super().__init__()
-        self.polybar_dir = environ.xdg_cfg_dir('polybar')
-        self.is_active = shutil.which('polybar') is not None\
-            and path.isdir(self.polybar_dir)
+
+    def _get_polybar_dir(self, cfg: ConfigParser):
+        return cfg[_KEY] if _KEY in cfg else environ.xdg_cfg_dir(_KEY)
+
+    def on_config_loaded(self, manager: ThemeManager):
+        self.polybar_dir = self._get_polybar_dir(manager.config)
+        self.launch_script = path.join(self.polybar_dir, 'launch2.sh')
+        self.is_active = path.isdir(self.polybar_dir)\
+            and path.isfile(self.launch_script)
 
     def on_startup(self, manager: ThemeManager):
-        process.run([path.join(self.polybar_dir, 'launch.sh')])
+        process.run([self.launch_script])
 
     def on_apply_theme(self, manager: ThemeManager):
         themeini_path = path.join(self.polybar_dir, 'theme2.ini')
         theme = manager.current_theme
 
+        pshade4 = theme.primary_colour.get_accent(theme.brightness)
+        pshade4text = pshade4.get_text_colour()
+        sshade4 = theme.secondary_colour.get_accent(theme.brightness)
+        # sshade4text = sshade4.get_text_colour()
+
         inidata = {
             'background': theme.background.to_hex(),
-            'foreground': theme.foreground.to_hex()
+            'foreground': theme.foreground.to_hex(),
+            'icons': sshade4.to_hex(),
+            'overline': pshade4.to_hex(),
+            'workspace-indicator': pshade4.to_hex(),
+            'workspace-indicator-text': pshade4text.to_hex(),
         }
-        inidata.update(theme.primary_colour.to_dict('primary-'))
-        inidata.update(theme.primary_colour.to_dict('secondary-'))
 
         cfg = ConfigParser()
         cfg['theme'] = inidata
         with open(themeini_path, 'wt') as f:
             cfg.write(f)
+        self.on_startup(manager)
